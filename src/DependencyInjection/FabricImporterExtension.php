4cdd70af048424f5de6a4bd531dbefe8bb8fd4f5
SLeussink <slk@fastbolt.com>
2025-11-18 10:01:00 +0100

    trying to fix DI

---
+++ src/DependencyInjection/FabricImporterExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fastbolt\FabricImporter\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class FabricImporterExtension extends Extension
+{
+    /**
+     * @param array           $configs
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yaml');
+    }
+}
